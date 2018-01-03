954d4ed0a4ab007e8bfc79f9ec514f3fb039afce
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-01-03 16:47:50 -0600

    Implementación de distintas notificaciones personalizadas, con una imagen grande, una lista, o un contenido muy grande.

--- app/src/main/java/com/shellcore/android/observerpattern/MainActivity.java
+++ app/src/main/java/com/shellcore/android/observerpattern/MainActivity.java
@@ -8,6 +8,7 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
+import android.widget.RemoteViews;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -39,6 +40,50 @@ public class MainActivity extends AppCompatActivity {
         sendNotification(order.update());
     }
 
+    @OnClick(R.id.btn_text)
+    public void onClickBtnText() {
+        NotificationCompat.BigTextStyle style = new NotificationCompat.BigTextStyle();
+
+        style.setBigContentTitle("Felicidades, tienes un bocadillo gratis");
+        style.setSummaryText("El próximo bocadillo que compres con Shell corre por nuestra cuenta!");
+        style.bigText("Como usuario número 1000 de la app, hemos decidido regalarte un bocadillo para la próxima vez que vengas a nuestra tienda. ¡Te esperamos!");
+
+        notificationId = 1;
+        sendNotification(style);
+    }
+
+    @OnClick(R.id.btn_image)
+    public void onClickBtnImage() {
+        NotificationCompat.BigPictureStyle style = new NotificationCompat.BigPictureStyle();
+        style.setBigContentTitle("Felicidades!!!");
+        style.setSummaryText("El próximo bocadillo que compres con Shell corre por nuestra cuenta!");
+        style.bigPicture(BitmapFactory.decodeResource(getResources(), R.drawable.sandwich));
+
+        notificationId = 2;
+        sendNotification(style);
+    }
+
+    @OnClick(R.id.btn_list)
+    public void onClickBtnList() {
+        NotificationCompat.InboxStyle style = new NotificationCompat.InboxStyle();
+        style.setBigContentTitle("Felicidades!!!");
+        style.setSummaryText("El próximo bocadillo que compres con Shell corre por nuestra cuenta!");
+
+        String[] list = {"Jamón", "Queso", "Aceitunas", "Atún", "Tomate", "Pimientos", "Cebollas"};
+        for (String s : list) {
+            style.addLine(s);
+        }
+
+        notificationId = 3;
+        sendNotification(style);
+    }
+
+    // Faltaría la notificación, además de la corrección de los errores
+    public void onInflateNotificationClicked() {
+        RemoteViews notificationLayout = new RemoteViews(getPackageName(), R.layout.activity_user_profile);
+        sendNotification(notificationLayout);
+    }
+
     private void sendNotification(String message) {
 
         Intent intent = new Intent(this, UserProfileActivity.class);
@@ -57,6 +102,30 @@ public class MainActivity extends AppCompatActivity {
                 .setTicker("Los mejores bocadillos de la ciudad")
                 .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.sandwich))
                 .setContentIntent(pendingIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        manager.notify(notificationId, builder.build());
+
+        notificationId++;
+    }
+
+    private void sendNotification(NotificationCompat.Style style) {
+        NotificationCompat.Builder builder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setStyle(style);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        manager.notify(notificationId, builder.build());
+
+        notificationId++;
+    }
+
+    private void sendNotification(RemoteViews view) {
+        NotificationCompat.Builder builder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContent(view);
 
         NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
